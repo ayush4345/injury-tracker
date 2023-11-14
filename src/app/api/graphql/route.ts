cf2f0eb9415da377dc7ecd3e09c14afd745b3961
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-11-14 15:05:53 +0530

    chnaged env variable

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,14 +1,9 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { resolvers } from "../../../../graphql/resolvers";
-// import { typeDefs } from "../../../../graphql/schema";
 import { createContext } from "../../../../graphql/context";
 import { schema } from "../../../../graphql/schema";
 
 const { handleRequest } = createYoga({
-  // schema: createSchema({
-  //   typeDefs,
-  //   resolvers,
-  // }),
   schema,
   graphqlEndpoint: "/api/graphql",
   context: createContext,
